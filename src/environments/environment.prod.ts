d2622a2e06dabfd1e67ac2df40cdfa582e50128b
unknown <danyharo@upeu.edu.pe>
2021-07-19 21:33:41 -0500

    make it better

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: false,
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  //configurando las credenciales de mi app-firebase a mi proyecto angular
+  firebaseConfig:{
+  apiKey: "AIzaSyBywBAuMN0BJukkR1hRDXJEIYOKKZM8COY",
+  authDomain: "exam3-24564.firebaseapp.com",
+  projectId: "exam3-24564",
+  storageBucket: "exam3-24564.appspot.com",
+  messagingSenderId: "19905998632",
+  appId: "1:19905998632:web:fa7470db2fec6ce7c12a1d",
+  measurementId: "G-TESBFVGT7J"
+  }
 };
